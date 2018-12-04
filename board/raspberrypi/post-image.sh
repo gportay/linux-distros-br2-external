3de393ec03bf4a4723f5609c305ff3ad8661d269
Gaël PORTAY <gael.portay@collabora.com>
2018-12-04 11:02:54 -0500

    raspberrypi: post-image.sh arguments as config.txt properties
    This patch improves the post-images script using the arguments given to
    the script as config.txt properties.
    
    As example, the arguments below:
    
    	--dtoverlay=miniuart-bt --dtparam=audio=on
    
    appends to:
    
    	dtoverlay=miniuart-bt
    	dtparam=audio=on
    
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- board/raspberrypi/post-image.sh
+++ board/raspberrypi/post-image.sh
@@ -31,10 +31,13 @@ arm_64bit=1
 __EOF__
 		fi
 		;;
-		--gpu_mem_256=*|--gpu_mem_512=*|--gpu_mem_1024=*)
-		# Set GPU memory
-		gpu_mem="${arg:2}"
-		sed -e "/^${gpu_mem%=*}=/s,=.*,=${gpu_mem##*=}," -i "${BINARIES_DIR}/rpi-firmware/config.txt"
+		--*=*)
+		# Set option=value
+		optval="${arg:2}"
+		sed -e "/^${opt%%=*}=/d" -i "${BINARIES_DIR}/rpi-firmware/config.txt"
+		cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
+$optval
+__EOF__
 		;;
 	esac
 
