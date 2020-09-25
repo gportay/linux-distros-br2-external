bbcc0aed251c187997daed3857f8f9870626272a
Gaël PORTAY <gael.portay@collabora.com>
2020-09-24 23:55:15 -0400

    package: add host-alpine-make-rootfs
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

---
+++ package/alpine-make-rootfs/alpine-make-rootfs.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# alpine-make-rootfs
+#
+################################################################################
+
+ALPINE_MAKE_ROOTFS_VERSION = 0.5.1
+ALPINE_MAKE_ROOTFS_SITE = $(call github,alpinelinux,alpine-make-rootfs,v$(ALPINE_MAKE_ROOTFS_VERSION))
+ALPINE_MAKE_ROOTFS_LICENSE = MIT
+ALPINE_MAKE_ROOTFS_LICENSE_FILES = LICENSE
+
+define HOST_ALPINE_MAKE_ROOTFS_INSTALL_CMDS
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) PREFIX=$(HOST_DIR) install
+endef
+
+$(eval $(host-generic-package))
