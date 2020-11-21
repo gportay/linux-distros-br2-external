32983911a6017176af4c7c30cae87334fb48fcea
Gaël PORTAY <gael.portay@collabora.com>
2020-11-21 03:38:19 -0500

    host-pacman-mirrorlist: bump to version 20201031-1 (x86_64)
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- package/pacman-mirrorlist/pacman-mirrorlist.mk
+++ package/pacman-mirrorlist/pacman-mirrorlist.mk
@@ -4,8 +4,9 @@
 #
 ################################################################################
 
-PACMAN_MIRRORLIST_VERSION = 20200606-1
-PACMAN_MIRRORLIST_SITE = https://www.archlinux.org/packages/core/any/pacman-mirrorlist/download/
+PACMAN_MIRRORLIST_VERSION = 20201031-1
+PACMAN_MIRRORLIST_SITE = http://mirrors.evowise.com/archlinux/core/os/x86_64
+PACMAN_MIRRORLIST_SOURCE= pacman-mirrorlist-$(PACMAN_MIRRORLIST_VERSION)-any.pkg.tar.zst
 ifeq ($(BR2_aarch64),y)
 PACMAN_MIRRORLIST_VERSION = 20200808-1
 PACMAN_MIRRORLIST_SITE = http://mirror.archlinuxarm.org/aarch64/core/
