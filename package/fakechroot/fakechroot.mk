4b59fe7c809d0a954706de9e456eb9176d4246ab
Gaël PORTAY <gael.portay@collabora.com>
2020-11-24 02:10:55 -0500

    fakechroot: fix substitution in arch-chroot environment
    This substitues the prefix /usr/local and fixes the use for the
    replacement.
    
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- package/fakechroot/fakechroot.mk
+++ package/fakechroot/fakechroot.mk
@@ -15,6 +15,7 @@ define HOST_FAKECHROOT_INSTALL_CMDS
 	$(INSTALL) -D -m644 $(HOST_FAKECHROOT_PKGDIR)/arch-chroot.env $(HOST_DIR)/etc/fakechroot/arch-chroot.env
 	$(INSTALL) -D -m755 $(HOST_FAKECHROOT_PKGDIR)/unshare.fakechroot.sh $(HOST_DIR)/bin/unshare.fakechroot
 	$(SED) 's,/usr/local,$(HOST_DIR),' $(HOST_DIR)/etc/fakechroot/pacstrap.env
+	$(SED) 's,/usr/local,$(HOST_DIR),' $(HOST_DIR)/etc/fakechroot/arch-chroot.env
 endef
 
 $(eval $(host-autotools-package))
