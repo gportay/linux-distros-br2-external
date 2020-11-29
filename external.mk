d54bb84730fd2012398ac631b871dc06d08adc65
Gaël PORTAY <gael.portay@collabora.com>
2020-11-29 06:55:42 -0500

    external.mk: add rm-build and rm-target rules
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- external.mk
+++ external.mk
@@ -1,1 +1,10 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_LINUX_DISTROS_PATH)/package/*/*.mk))
+
+.PHONY: rm-build
+rm-build:
+	rm -rf $(BUILD_DIR)/*/*
+
+.PHONY: rm-target
+rm-target:
+	rm -rf $(BASE_TARGET_DIR)
+	rm -rf $(BUILD_DIR)/*/.stamp_target_installed
