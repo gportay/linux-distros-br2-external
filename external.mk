a6b2a12edadeefaf37f3a35043d808f5130d5fc7
Gaël PORTAY <gael.portay@collabora.com>
2020-12-20 12:57:32 -0500

    external.mk: add target-add-ssh-authorized-keys rule
    This adds the rule target-add-ssh-authorized-keys that sets the public
    keys that are currently represented by the agent to the root user.
    
    Signed-off-by: Gaël PORTAY <gael.portay@collabora.com>

--- external.mk
+++ external.mk
@@ -1,5 +1,13 @@
 include $(sort $(wildcard $(BR2_EXTERNAL_LINUX_DISTROS_PATH)/package/*/*.mk))
 
+.PHONY: target-add-ssh-authorized-keys
+target-add-ssh-authorized-keys:
+	$(Q)if which ssh-add >/dev/null 2>&1; then \
+		$(call MESSAGE,"Add SSH authorized keys"); \
+		install -d -m750 $(TARGET_DIR)/root/.ssh; \
+		ssh-add -L > $(TARGET_DIR)/root/.ssh/authorized_keys; \
+	fi
+
 .PHONY: rm-build
 rm-build:
 	rm -rf $(BUILD_DIR)/*/*
