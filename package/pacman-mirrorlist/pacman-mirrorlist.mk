6e05c1cc154cc09577524a581821216a95f58bd5
Gaël PORTAY <gael.portay@collabora.com>
2021-12-14 19:23:18 +0100

    host-pacman-mirrorlist: bump to version 20211120-1 (aarch64)

--- package/pacman-mirrorlist/pacman-mirrorlist.mk
+++ package/pacman-mirrorlist/pacman-mirrorlist.mk
@@ -8,7 +8,7 @@ PACMAN_MIRRORLIST_VERSION = 20211212-1
 PACMAN_MIRRORLIST_SITE = http://mirror.rackspace.com/archlinux/core/os/x86_64
 PACMAN_MIRRORLIST_SOURCE= pacman-mirrorlist-$(PACMAN_MIRRORLIST_VERSION)-any.pkg.tar.zst
 ifeq ($(BR2_aarch64)$(BR2_arm),y)
-PACMAN_MIRRORLIST_VERSION = 20200808-1
+PACMAN_MIRRORLIST_VERSION = 20211120-1
 PACMAN_MIRRORLIST_SITE = http://mirror.archlinuxarm.org/aarch64/core/
 PACMAN_MIRRORLIST_SOURCE= pacman-mirrorlist-$(PACMAN_MIRRORLIST_VERSION)-any.pkg.tar.xz
 endif
