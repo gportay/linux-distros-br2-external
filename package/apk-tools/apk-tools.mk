2566afacaf80e9c92a5878023352ac1ca5316fec
Gaël PORTAY <gael.portay@collabora.com>
2021-12-17 04:13:22 +0100

    host-apk-tools: bump to version 2.12.8

--- package/apk-tools/apk-tools.mk
+++ package/apk-tools/apk-tools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-APK_TOOLS_VERSION = 2.10.5
+APK_TOOLS_VERSION = 2.12.8
 APK_TOOLS_SITE = $(call github,alpinelinux,apk-tools,v$(APK_TOOLS_VERSION))
 APK_TOOLS_LICENSE = GPL-2.0
 APK_TOOLS_LICENSE_FILES = LICENSE
@@ -16,7 +16,7 @@ define HOST_APK_TOOLS_BUILD_CMDS
 endef
 
 define HOST_APK_TOOLS_INSTALL_CMDS
-	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) LUAAPK= SBINDIR=$(HOST_DIR)/sbin LIBDIR=$(HOST_DIR)/lib CONFDIR=$(HOST_DIR)/etc/apk MANDIR=$(HOST_DIR)/share/man DOCDIR=$(HOST_DIR)/share/doc/apk CFLAGS=$(HOST_CFLAGS) LDFLAGS="$(HOST_LDFLAGS)" install
+	$(HOST_MAKE_ENV) $(MAKE) -C $(@D) LUAAPK= SBINDIR=$(HOST_DIR)/sbin LIBDIR=$(HOST_DIR)/lib CONFDIR=$(HOST_DIR)/etc/apk MANDIR=$(HOST_DIR)/share/man DOCDIR=$(HOST_DIR)/share/doc/apk CFLAGS=$(HOST_CFLAGS) LDFLAGS="$(HOST_LDFLAGS)" INCLUDEDIR=$(HOST_DIR)/include PKGCONFIGDIR=$(HOST_DIR)/lib/pkgconfig install
 endef
 
 $(eval $(host-generic-package))
