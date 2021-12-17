d898b1bbb95e9c2d013b9fca204e45c9884b4e74
Gaël PORTAY <gael.portay@collabora.com>
2021-12-17 04:17:56 +0100

    host-alpine-make-rootfs: bump to version 0.6.0

--- package/alpine-make-rootfs/alpine-make-rootfs.mk
+++ package/alpine-make-rootfs/alpine-make-rootfs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALPINE_MAKE_ROOTFS_VERSION = 0.5.1
+ALPINE_MAKE_ROOTFS_VERSION = 0.6.0
 ALPINE_MAKE_ROOTFS_SITE = $(call github,alpinelinux,alpine-make-rootfs,v$(ALPINE_MAKE_ROOTFS_VERSION))
 ALPINE_MAKE_ROOTFS_LICENSE = MIT
 ALPINE_MAKE_ROOTFS_LICENSE_FILES = LICENSE
